7e3f51a476ed8f5fdbc41aaf6bf80ac7862c3ae3
RunarReve <reverunar@outlook.com>
2020-03-12 15:59:56 +0300

    Set up the directory

---
+++ compare.py
@@ -0,0 +1,12 @@
+#!/bin/env/python
+#Will compare two assoc to see if there is different and similar peaks
+#argv[1] assoc 1
+#argv[2] assoc 2
+
+def main:
+
+
+
+
+if __name__ == "__main__":
+  main()
