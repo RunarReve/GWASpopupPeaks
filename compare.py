aa4a53bf295ab9980f24098d55e1043b02db86cd
RunarReve <reverunar@outlook.com>
2020-03-12 17:28:48 +0300

    Think get peaks from assoc

--- compare.py
+++ compare.py
@@ -10,7 +10,10 @@ def main():
    a2 = [[] for _ in range(amountCHR)]
    a1sum = 0
    a2sum = 0
- 
+   peakpoint = 0.15
+   a1name = sys.argv[1].split('/')[-1]
+   a2name = sys.argv[2].split('/')[-1]
+
 
    p = 9e-9
    for line in open(sys.argv[1]):
@@ -37,10 +40,25 @@ def main():
       a2[int(sline[0])-1].append(sline)
       a2sum += 1 
 
-   for each in a1:
-      for beach in each:
-         print(beach)
-   
+   #How to define a peak easily
+   #When one chromosome is <15% of the assoc
+   for i, a1l, a2l in zip(range(amountCHR), a1, a2):
+      a1p = what(len(a1l), a1sum)
+      a2p = what(len(a2l), a2sum)
+      if(a1p > peakpoint):
+         print(a1name+" parent has peak at CHR: "+str(i)+" it's "+str(a1p)+" of the assoc")
+      if(a2p > peakpoint):
+         print(a2name+" child  has peak at CHR: "+str(i)+" it's "+str(a2p)+" of the assoc")
+      
+#      print(str(i+1) +' '+str(len(a1l))+' '+a1p+'\t'+str(len(a2l))+' '+a2p )
+
+
+#Will find the persentage of given value is of the tot
+def what(dis, tot): 
+   if(dis == 0):
+      return 0 
+   #print(str(dis) + '/'+str(tot)+' = '+ str(float(dis)/tot))
+   return float(dis)/tot
 
 if __name__ == "__main__":
   main()
