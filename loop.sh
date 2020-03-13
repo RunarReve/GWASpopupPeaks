630437aee7cdfe624e45997ac7ceab453407ef4d
RunarReve <reverunar@outlook.com>
2020-03-13 13:56:08 +0300

    Fixed the combination

--- loop.sh
+++ loop.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 #Loops through all GWASs that was made with HPOscase/HPOsiblingcontrol
 
+rm -f combined
 
-for each in $(ls output/sib):
+for each in $(wc -l output/sib/* | awk '{if($1 > 1){print $2}}' )
 do
-   #echo "${each}"
+   echo "${each}"
+   string=$(echo "${each}" | sed -e 's/\// /g' | awk '{print $NF}')
+   same="x"
+   diff="x"
 
-   cat output/sib/${each} | sed -e 's/newPeak/newPeak\n/g' | sed -e 's/samePeak/samePeak\n/g' > output/sib/${each}T
-   mv output/sib/${each}T output/sib/${each} 
+   #Get all peaks
+   for i in $(grep parent_peak ${each}| awk '{print $4}');do
+      string="${string},${i}"
+   done
+   for i in $(grep samePeak ${each} | awk '{print $1","$4}'); do
+      same="${same}_${i}"
+   done
+   for i in $(grep newPeak  ${each} | awk '{print $1","$4}'); do
+      diff="${diff}_${i}"
+   done
+   #Remove default value if anything else is added 
+   same=$(echo "${same}" |sed -e 's/x_//g')
+   diff=$(echo "${diff}" |sed -e 's/x_//g')
 
-   #if [ $(wc -l output/sib/${each}| awk '{print $1}') == '0' ]; then 
-   #   rm output/sib/${each}
-   #fi
+   string="${string}	${same}	${diff}"
+   
+   echo "${string}" >> combined
 done
